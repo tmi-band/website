053cdb577cce79513fbce8dd8f9076a72b1a8011
Sarah Roberts <sarahr@cyverse.org>
2019-02-14 22:49:53 -0700

    added a script for loading events to make them easier to add and remove

---
+++ events.js
@@ -0,0 +1,92 @@
+function addEvents(tableId, events) {
+  let tableRef = document.getElementById(tableId);
+
+  // Add each of the events to the table.
+  let index = 0;
+  for (const e of events) {
+    index += 1;
+
+    // Insert a new row.
+    row = tableRef.insertRow(-1);
+    row.className = (index % 2 == 0) ? "event-table-even" : "event-table-odd";
+
+    // Add the event date cell to the row.
+    dateCell = row.insertCell(0);
+    dateCell.className = "event-date";
+    dateCell.appendChild(document.createTextNode(e.date));
+
+    // Add the event location cell to the row.
+    locationCell = row.insertCell(1);
+    locationCell.className = "event-location";
+
+    // Add the venue name to the event location cell.
+    venueName = document.createElement("p");
+    venueName.className = "venue-name";
+    venueName.appendChild(document.createTextNode(e.venue.name));
+    locationCell.appendChild(venueName);
+
+    // Add the venue address to the event location cell.
+    venueAddress = document.createElement("p");
+    venueAddress.className = "venue-address";
+    venueAddress.appendChild(document.createTextNode(e.venue.address));
+    locationCell.appendChild(venueAddress);
+
+    // Add the cost and time cell to the row.
+    costAndTimeCell = row.insertCell(2);
+
+    // Add the cost to the cost and time cell.
+    eventCost = document.createElement("p");
+    eventCost.className = "event-cost";
+    eventCost.appendChild(document.createTextNode(e.cost));
+    costAndTimeCell.appendChild(eventCost);
+
+    // Add the time to the cost and time cell.
+    eventTime = document.createElement("p");
+    eventTime.className = "event-time";
+    eventTime.appendChild(document.createTextNode(e.time));
+    costAndTimeCell.appendChild(eventTime);
+  }
+}
+
+const venues = {
+  "macaroni_grill": {
+    "name": "Macaroni Grill",
+    "address": "5100 E Broadway Blvd, Tucson, AZ"
+  },
+  "hacienda_del_lago": {
+    "name": "Hacienda del Lago Golf Course",
+    "address": "14155 Via Rancho del Lago, Vail, AZ"
+  },
+  "house_of_bards": {
+    "name": "House of Bards",
+    "address": "4915 E Speedway Blvd, Tucson, AZ"
+  }
+}
+
+const events = [
+  {
+    "date": "Feb 16",
+    "time": "7:00 - 9:00",
+    "cost": "Free",
+    "venue": venues.hacienda_del_lago
+  },
+  {
+    "date": "Mar 7",
+    "time": "6:00 - 9:00",
+    "cost": "Free",
+    "venue": venues.macaroni_grill
+  },
+  {
+    "date": "Mar 9",
+    "time": "7:00 - 9:00",
+    "cost": "Free",
+    "venue": venues.hacienda_del_lago
+  },
+  {
+    "date": "Mar 15",
+    "time": "8:00 - 12:00",
+    "cost": "Cover",
+    "venue": venues.house_of_bards
+  }
+];
+addEvents("event-table", events);
